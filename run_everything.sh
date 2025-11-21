b12b8057bd6a2baa0cf1d24114ed049820b5e1dc
Ubuntu <ubuntu@ip-172-31-32-31.us-east-2.compute.internal>
2025-11-21 14:52:01 +0000

    ___

--- run_everything.sh
+++ run_everything.sh
@@ -4,20 +4,20 @@
 # echo "evaluate cls model"
 # python3 eval_cls.py --exp_name both
 
-echo "run seg training"
-python3 train.py --task seg
+# echo "run seg training"
+# python3 train.py --task seg
 
-echo "evaluate seg model"
-python3 eval_seg.py --exp_name both
+# echo "evaluate seg model"
+# python3 eval_seg.py --exp_name both
 
 echo "run cls_dgcnn training"
-python3 train.py --task cls_dgcnn
+python3 train.py --task cls_dgcnn --num_points 4096 --batch_size 16
 
 echo "evaluate cls_dgcnn model"
 python3 eval_cls.py --exp_name both --dgcnn
 
 echo "run seg_dgcnn training"
-python3 train.py --task seg_dgcnn
+python3 train.py --task seg_dgcnn --num_points 2048 --batch_size 16
 
 echo "evaluate seg_dgcnn model"
 python3 eval_seg.py --exp_name both --dgcnn
